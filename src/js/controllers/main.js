a6dde87b32ef341711fea0900d88a145f24f01fe
Huw Fernie <HuwFernie@gmail.com>
2017-04-15 14:15:30 +0100

    updated main controller for auth error broadcasting

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -2,11 +2,14 @@ angular
   .module('yabee')
   .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['User', 'Advert', 'Offer', 'Message'];
-function MainCtrl(User, Advert, Offer, Message) {
+MainCtrl.$inject = ['User', 'Advert', 'Offer', 'Message', '$rootScope'];
+function MainCtrl(User, Advert, Offer, Message, $rootScope) {
   const vm = this;
   vm.users = User.query();
   vm.adverts = Advert.query();
   vm.offers = Offer.query();
   vm.messages = Message.query();
+  $rootScope.$on('error', (e, err) => {
+    console.log(e, err);
+  });
 }
