5a9cde44229ea2af5fdd4989b412a2f2361d2b80
Huw Fernie <HuwFernie@gmail.com>
2017-04-15 13:03:33 +0100

    setting up ui-router

--- src/js/controllers/MainCtrl.js
+++ src/js/controllers/MainCtrl.js
@@ -2,9 +2,11 @@ angular
   .module('yabee')
   .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['User', 'Advert'];
-function MainCtrl(User, Advert) {
+MainCtrl.$inject = ['User', 'Advert', 'Offer', 'Message'];
+function MainCtrl(User, Advert, Offer, Message) {
   const vm = this;
   vm.users = User.query();
   vm.adverts = Advert.query();
+  vm.offers = Offer.query();
+  vm.messages = Message.query();
 }
