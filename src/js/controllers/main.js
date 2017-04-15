0995d0acaa9b28d386c16fc90f6dfa025420a012
Huw Fernie <HuwFernie@gmail.com>
2017-04-15 14:21:53 +0100

    added error message to index.html and changed main controller to work with it

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -2,14 +2,15 @@ angular
   .module('yabee')
   .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['User', 'Advert', 'Offer', 'Message', '$rootScope'];
-function MainCtrl(User, Advert, Offer, Message, $rootScope) {
+MainCtrl.$inject = ['User', 'Advert', 'Offer', 'Message', '$rootScope', '$state'];
+function MainCtrl(User, Advert, Offer, Message, $rootScope, $state) {
   const vm = this;
   vm.users = User.query();
   vm.adverts = Advert.query();
   vm.offers = Offer.query();
   vm.messages = Message.query();
   $rootScope.$on('error', (e, err) => {
-    console.log(e, err);
+    vm.message = err.data.message;
+    $state.go('login');
   });
 }
