e24bdc58c62c3f1d30627aaab86a886dd0ff943b
Huw Fernie <HuwFernie@gmail.com>
2017-04-15 12:50:49 +0100

    offer factory finished

--- src/js/controllers/MainCtrl.js
+++ src/js/controllers/MainCtrl.js
@@ -2,8 +2,9 @@ angular
   .module('yabee')
   .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['User'];
-function MainCtrl(User) {
+MainCtrl.$inject = ['User', 'Advert'];
+function MainCtrl(User, Advert) {
   const vm = this;
   vm.users = User.query();
+  vm.adverts = Advert.query();
 }
