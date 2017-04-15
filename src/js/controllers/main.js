f418c180c8849f3b134e965ef987e10e52f8bd87
Huw Fernie <HuwFernie@gmail.com>
2017-04-15 14:51:56 +0100

    should have the front end finished for login/register/logout... need to finish backend to check it

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -2,19 +2,30 @@ angular
   .module('yabee')
   .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['User', 'Advert', 'Offer', 'Message', '$rootScope', '$state'];
-function MainCtrl(User, Advert, Offer, Message, $rootScope, $state) {
+MainCtrl.$inject = ['User', 'Advert', 'Offer', 'Message', '$rootScope', '$state', '$auth'];
+function MainCtrl(User, Advert, Offer, Message, $rootScope, $state, $auth) {
   const vm = this;
+
+  vm.isAuthenticated = $auth.isAuthenticated;
+
   vm.users = User.query();
   vm.adverts = Advert.query();
   vm.offers = Offer.query();
   vm.messages = Message.query();
+
   $rootScope.$on('error', (e, err) => {
     vm.message = err.data.message;
-    $state.go('login');
+    if(err.status === 401) $state.go('login');
   });
   $rootScope.$on('$stateChangeSuccess', () => {
     if(vm.stateHasChanged) vm.message = null;
     if(!vm.stateHasChanged) vm.stateHasChanged = true;
   });
+
+  function logout() {
+    $auth.logout();
+    $state.go('login');
+  }
+
+  vm.logout = logout;
 }
