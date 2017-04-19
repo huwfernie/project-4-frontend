9de8850fcb9f2c000c06598a9bf8a2873376b646
Huw Fernie <HuwFernie@gmail.com>
2017-04-19 10:22:48 +0100

    messages on usersShow are being joined into currentUser.messages and then displayed/and ordered by

--- src/js/controllers/users.js
+++ src/js/controllers/users.js
@@ -3,30 +3,23 @@ angular
   .controller('usersShowCtrl', usersShowCtrl)
   .controller('usersIndexCtrl', usersIndexCtrl);
 
-usersIndexCtrl.$inject = ['User', 'Advert', 'Offer', 'Message'];
-function usersIndexCtrl(User, Advert, Offer, Message) {
+usersIndexCtrl.$inject = ['User'];
+function usersIndexCtrl(User) {
   const vm = this;
   vm.users = User.query();
-  vm.adverts = Advert.query();
-  vm.offers = Offer.query();
-  vm.messages = Message.query();
 }
 
-usersShowCtrl.$inject = ['User', 'Advert', 'Offer', 'Message', '$auth', '$http', '$stateParams'];
-function usersShowCtrl(User, Advert, Offer, Message, $auth, $http, $stateParams) {
+usersShowCtrl.$inject = ['User', '$auth', '$http', '$stateParams'];
+function usersShowCtrl(User, $auth, $http, $stateParams) {
   const vm = this;
 
-  // switch between these two so that usersShow will only show the currently logged in user.
-  // if ($auth.getPayload()) vm.currentUser = User.get({ id: $auth.getPayload().id });
-  vm.currentUser = User.get($stateParams);
 
-  // I don't need this because the data is already embedded in the user JSON from the server.
-  // $http({
-  //   url: '//localhost:3000/api/offers/myOffers',
-  //   method: 'GET',
-  //   params: {id: vm.currentUser.id}
-  // })
-  // .then((response) => {
-  //   vm.offers = response.data;
-  // });
+  User.get($stateParams)
+  .$promise
+  .then((user) => {
+    vm.currentUser = user;
+    vm.currentUser.messages = [];
+    vm.currentUser.messages_sent.forEach((message) => vm.currentUser.messages.push(message));
+    vm.currentUser.messages_recieved.forEach((message) => vm.currentUser.messages.push(message));
+  });
 }
